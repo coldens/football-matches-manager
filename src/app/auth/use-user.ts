6a21487155328791df14c9fa26198623c2b18011
Jose Gonzalez <jdgg2414@gmail.com>
2024-09-04 21:27:55 -0500

    Update dependencies and configuration files

--- src/app/auth/use-user.ts
+++ src/app/auth/use-user.ts
@@ -3,6 +3,6 @@ import { User } from '@prisma/client';
 import { Request } from 'express';
 
 export const UseUser = createParamDecorator((_: unknown, ctx: ExecutionContext): User => {
-  const auth = ctx.switchToHttp().getRequest<Request>().user;
+  const auth = ctx.switchToHttp().getRequest<Request>().user!;
   return auth;
 });
